706cde0d1b6288b672ad2ad3ce355664e9540fee
Jonas Johansson <jns.johansson@gmail.com>
2022-01-10 08:03:02 +0100

    socket-listener is now integrated with oscclient_demo

--- server-pythonosc/socket-listener.py
+++ server-pythonosc/socket-listener.py
@@ -1,13 +1,27 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+import argparse
+import random
+import time
+import json
+import websocket
 
-import asyncio
-import websockets
+from pythonosc import udp_client
+
+server_address = 'wss://womu-server.jonasjohansson.repl.co'
+ip = "127.0.0.1"
+port = 7500
+
+client = udp_client.SimpleUDPClient(ip, port)
 
 
-async def hello():
-    async with websockets.connect('wss://womu-server.jonasjohansson.repl.co') as websocket:
+def on_message(wsapp, message):
+    msg = json.loads(message)
+    addr = msg["address"]
+    val = msg["value"]
+    print(addr, val)
+    client.send_message(addr, val)
 
-        greeting = await websocket.recv()
-        print("< {}".format(greeting))
 
-asyncio.get_event_loop().run_until_complete(hello())
+wsapp = websocket.WebSocketApp(
+    server_address, on_message=on_message)
+wsapp.run_forever()
